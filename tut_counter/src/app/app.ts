2dfea8a5963e0194ed3ce9688dc06085820a0422
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-06-19 20:24:48 +0530

    Added the counter tutorial

---
+++ tut_counter/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('tut_counter');
+  count:number = 0;
+  counter(){
+    this.count ++;
+  }
+}
